a5f65162f80cb51e03e6d5f383273c4609668754
nikita <nvasiuta@brooklynsteamcenter.org>
2023-05-15 14:55:33 -0400

    almost reached other people

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+import {ref} from 'vue'
+let todos = ref(['','',''])
+
+function addTodo () {
+todos.value.push({
+	text: newTodo.value,
+	complete: false
+})
+	newTodo.value = ''
+}
+
+
+
+
+
+
+
+</script>
+
+<template>
+  <h1>My Todo Application</h1>
+  <li v-for="todo in todos">
+<input type="checkbox" v-model="todo.complete">
+
+{{todo.text}}
+
+<button @click="delete">Delete</button>
+
+  </li>
+  <input v-model="newTodo">
+  <button @click="add">Add Todo</button>
+</template>
+
+<style scoped>
+
+</style>
